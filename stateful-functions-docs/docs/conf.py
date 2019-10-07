49bae9f16210bc4c1ad5abebdcbfd2ac34d5af68
Stephan Ewen <sewen@apache.org>
2019-10-07 21:39:17 +0200

    [build] release commit for 1.0.0

--- stateful-functions-docs/docs/conf.py
+++ stateful-functions-docs/docs/conf.py
@@ -61,7 +61,7 @@ html_show_copyright = False
 # built documents.
 #
 # The short X.Y version.
-version = u'1.0-SNAPSHOT'
+version = u'1.0'
 # The full version, including alpha/beta/rc tags.
 release = u'1.0.0'
 # The latest Flink version supported by Stateful Functions
